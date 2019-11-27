078f39b9f9a7c5df9393dd969e83b76f8f941e50
Prawee Wongsa <prawee@hotmail.com>
2019-11-28 02:09:38 +0700

    using body-parser middleware for json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(authRoutes)
 
 const mongoUri = `mongodb+srv://admin:p@ssw0rd@development-vt4z8.mongodb.net/test?retryWrites=true&w=majority`
