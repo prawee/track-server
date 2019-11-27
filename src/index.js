ea84a3e407ed6a224993511dc1c984f0920e1c83
Prawee Wongsa <prawee@hotmail.com>
2019-11-28 04:57:04 +0700

    import tracks route to app

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(authRoutes)
+app.use(trackRoutes)
 
 const mongoUri = `mongodb+srv://admin:p@ssw0rd@development-vt4z8.mongodb.net/uat?retryWrites=true&w=majority`
 mongoose.connect(mongoUri, {
